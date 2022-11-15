3fe8b5ba1fbfe619eb3e7cf82bca24849b74100d
tomi204 <tomi204@outlook.com>
2022-11-15 13:28:55 -0300

    update contract

--- contracts/ERC721A.sol
+++ contracts/ERC721A.sol
@@ -1,11 +1,30 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "erc721a/contracts/ERC721A.sol";
 
-contract Tomi204{
-    constructor() {
-        
+contract Tomi204 is ERC721A {
+    uint256 token_count;
+
+    constructor() ERC721A("Tomi204", "TOMI204") {}
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        return
+            "https://ipfs.io/ipfs/QmXMjXEjWBAzfA3UGPEQPub8iZAqD8myK12jKpE4F9EGfe";
+    }
+
+    function mint(uint256 quantity) external payable {
+        token_count += 1;
+        _mint(msg.sender, quantity);
     }
 }
-
